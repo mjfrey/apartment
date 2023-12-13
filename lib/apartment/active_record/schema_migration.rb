fe0a5f84ef87f0a57eb8b2a72e69de7b0ca61af2
Michael Frey <michaeljfrey@gmail.com>
2023-12-13 11:53:13 -0500

    fixup

--- lib/apartment/active_record/schema_migration.rb
+++ lib/apartment/active_record/schema_migration.rb
@@ -4,7 +4,7 @@ module ActiveRecord
   class SchemaMigration < ActiveRecord::Base.connection.schema_migration # :nodoc:
     class << self
       def table_exists?
-        connection.table_exists?(table_name)
+        table_exists?(table_name)
       end
     end
   end
