fe312d8b6f1ce158ab7e0c00f50deee26c20c815
Michael Frey <michaeljfrey@gmail.com>
2023-12-14 15:27:12 -0500

    fixup

--- lib/apartment/active_record/schema_migration.rb
+++ lib/apartment/active_record/schema_migration.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
-
+# nodoc
 module ActiveRecord
-  class SchemaMigration # :nodoc:
-    class << self
-      def table_exists?
-        connection.table_exists?(table_name)
-      end
+  SchemaMigration.class_eval do # nodoc
+    def table_exists?
+      connection.table_exists?(table_name)
     end
   end
 end
